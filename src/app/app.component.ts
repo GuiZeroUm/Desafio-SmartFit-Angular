57c80e6fbe686ecf79e56ccbc10ebcc1fab067fc
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2024-02-18 13:47:53 -0500

    feat: add form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormsComponent } from "./components/forms/forms.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, FormsComponent]
+    imports: [RouterOutlet, HeaderComponent, FormsComponent, ReactiveFormsModule]
 })
 export class AppComponent {
   title = 'desafio-smartfit-angular';
